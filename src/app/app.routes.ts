95de6d84cced9d8e6ca7b2d1b244d6033ba27187
Martin Lapihuska <madalucard@gmail.com>
2024-08-06 11:12:33 +0200

    quick defualt routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  //{ path: 'heroes', component:  },
+  //{ path: 'story', component:  },
+  //{ path: 'tournaments', component:  },
+  //{ path: 'ambassador', component:  },
+  { path: '**', redirectTo: '' },
+];
